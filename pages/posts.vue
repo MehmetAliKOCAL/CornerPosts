8fe53b1050b777964740834e3d9858c7519566ab
Mehmet Ali KOÇAL <root.gwyn@hotmail.com>
2023-04-15 23:50:50 +0300

    toast messages added on post delete and post update

--- pages/posts.vue
+++ pages/posts.vue
@@ -1,7 +1,22 @@
 <script setup>
+import Swal from "sweetalert2";
+import posts from "/utilities/posts.js";
 definePageMeta({
   layout: "posts",
 });
+
+if (posts.messageToShow != "") {
+  Swal.fire({
+    title: posts.messageToShow,
+    toast: true,
+    position: "bottom-end",
+    showConfirmButton: false,
+    timer: "2500",
+    background: "#1BA31F",
+    color: "white",
+  });
+  posts.messageToShow = "";
+}
 </script>
 <template>
   <NuxtPage />
--- pages/posts/[id].vue
+++ pages/posts/[id].vue
@@ -1,6 +1,21 @@
 <script setup>
+import Swal from "sweetalert2";
 import posts from "/utilities/posts.js";
-const post = posts[useRoute().params.id];
+const allPosts = posts.allPosts;
+const post = allPosts[useRoute().params.id];
+
+if (posts.messageToShow != "") {
+  Swal.fire({
+    title: posts.messageToShow,
+    toast: true,
+    position: "bottom-end",
+    showConfirmButton: false,
+    timer: "2500",
+    background: "#1BA31F",
+    color: "white",
+  });
+  posts.messageToShow = "";
+}
 </script>
 <template>
   <section class="my-10 max-w-[1400px] px-6 mx-auto">
