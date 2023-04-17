9ece20f6118eed04da31047a3bf10321e69ccf04
Mehmet Ali KOÇAL <root.gwyn@hotmail.com>
2023-04-17 19:51:55 +0300

    bug fix that causing the post count to not show up

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -5,7 +5,7 @@ import posts from "/utilities/posts.js";
   <section class="px-6 max-w-[1400px] mx-auto my-10">
     <h2 class="font-bold text-xl">All Posts</h2>
     <p class="text-slate-400 font-medium mb-10">
-      Currently {{ posts.length }} posts available
+      Currently {{ posts.allPosts.length }} posts available
     </p>
     <div class="flex flex-wrap gap-2">
       <Cards :admin="false" />
