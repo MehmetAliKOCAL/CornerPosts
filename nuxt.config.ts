7c79ef184db320523afbe30711343d6953a7bd06
Mehmet Ali KOÇAL <root.gwyn@hotmail.com>
2023-04-24 00:07:39 +0300

    installed pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: [
-      '@nuxtjs/tailwindcss'
+      '@nuxtjs/tailwindcss', '@pinia/nuxt',
       ],
+
       css: [
         '@/assets/css/global.css',
       ],
+      
+      runtimeConfig:{
+        FIREBASE_URL:process.env.FIREBASE_URL,
+      },
+
       app: {
         pageTransition: { name: 'slide', mode: 'out-in' },
         layoutTransition: { name: 'layout', mode: 'out-in' },
